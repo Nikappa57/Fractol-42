bac7d8c3da54f58a41bfcd462c44d9f8d929b2a4
Nikappa57 <lorenz.gaudino@gmail.com>
2023-04-16 19:54:42 +0200

    fix info

--- srcs/main/info.c
+++ srcs/main/info.c
@@ -6,7 +6,7 @@
 /*   By: lorenzogaudino <lorenzogaudino@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 01:35:08 by lorenzogaud       #+#    #+#             */
-/*   Updated: 2023/04/16 19:42:51 by lorenzogaud      ###   ########.fr       */
+/*   Updated: 2023/04/16 19:53:35 by lorenzogaud      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	set_info(t_vars *vars)
 	t_info	info;
 
 	info.iter = ft_itoa(vars->frctl->maxiter);
-	if (vars->frctl->maxiter > MAX_ITER)
+	if (vars->frctl->maxiter >= MAX_ITER)
 		info.iter = ft_strjoin_gnl(info.iter, " [MAX]");
 	info.inc = ft_strjoin_gnl(ft_itoa(vars->frctl->inc * 100), "/100");
 	info.radius = ft_itoa(sqrt(vars->frctl->radius));
@@ -27,7 +27,7 @@ void	set_info(t_vars *vars)
 	mlx_string_put(vars->mlx, vars->win, 15, 30, WHITE, "Zoom:");
 	mlx_string_put(vars->mlx, vars->win, 15, 45, WHITE, "Inc:");
 	mlx_string_put(vars->mlx, vars->win, 15, 60, WHITE, "Radius:");
-	mlx_string_put(vars->mlx, vars->win, 15, 75, WHITE, "Color:");
+	mlx_string_put(vars->mlx, vars->win, 15, 75, WHITE, "Palette:");
 	mlx_string_put(vars->mlx, vars->win, 80, 15, WHITE, info.iter);
 	mlx_string_put(vars->mlx, vars->win, 80, 30, WHITE, info.zoom);
 	mlx_string_put(vars->mlx, vars->win, 80, 45, WHITE, info.inc);
