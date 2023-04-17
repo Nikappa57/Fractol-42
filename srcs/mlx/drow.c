8bd49278dce6e1a4e8742c1fc214d2fd09b1278f
Lorenzo Gaudino <49768358+Nikappa57@users.noreply.github.com>
2023-04-17 13:09:30 +0200

    new img fix

--- srcs/mlx/drow.c
+++ srcs/mlx/drow.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   drow.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lorenzogaudino <lorenzogaudino@student.    +#+  +:+       +#+        */
+/*   By: lgaudino <lgaudino@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 23:41:48 by lorenzogaud       #+#    #+#             */
-/*   Updated: 2023/04/16 19:36:10 by lorenzogaud      ###   ########.fr       */
+/*   Updated: 2023/04/17 12:47:08 by lgaudino         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ void	ft_mlx_window_reload(t_vars *vars)
 
 	img = vars->img;
 	mlx_destroy_image(vars->mlx, img->img_ptr);
-	img->img_ptr = mlx_new_image(vars->win, WINDOW_WIDTH, WINDOW_HEIGHT);
+	img->img_ptr = mlx_new_image(vars->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	img->addr = mlx_get_data_addr(img->img_ptr, &img->bits_per_pixel,
 			&img->line_length, &img->endian);
 	show_frctl(vars);
